d93fc9d271f311710c7e69b7a6d08cefffdae8dd
Cameron Flowers <cameron.flowers13@gmail.com>
2021-09-27 18:58:42 -0500

    adding lesson material

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,7 @@
+import React from 'react'
 
 function Hello() {
-  return (
-    <div>
-        Yoooooooo we hacking on React!
-    </div>
-  );
+  return <div>Yoooooooo we hacking on React!</div>
 }
 
-export default Hello;
+export default Hello
